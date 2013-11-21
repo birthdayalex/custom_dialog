94283568d6d9caa93487f3a77ecb548d66f881c6
Slidenerd <slidenerd@gmail.com>
2013-11-21 23:05:37 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package slidenerd.vivz;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements MyDialog.Communicator{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+    public void showDialog(View v)
+    {
+        FragmentManager manager=getFragmentManager();
+        MyDialog myDialog=new MyDialog();
+        myDialog.show(manager,"MyDialog");
+    }
+
+    @Override
+    public void onDialogMessage(String message) {
+        Toast.makeText(this, message,Toast.LENGTH_SHORT).show();
+    }
+}
